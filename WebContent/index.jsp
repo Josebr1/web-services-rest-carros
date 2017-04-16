ca1aecb977a6ad219ef0bbdd2e749cd5cde9eb3f
Jose <jose.silva.br@outlook.com>
2017-04-15 21:26:44 -0300

    Passagem de parâmetros por formulário

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,1 +1,13 @@
-Olá, mundo!
+<form method="post" action="<%=request.getContextPath()%>/hello" >
+
+	Nome: <input type="text" name="nome" />
+	
+	<br/><br/>
+	
+	Sobrenome: <input type="text" name="sobrenome" />
+	
+	<br/>
+	
+	<input type="submit" name="Enviar" />
+
+</form>
--- src/br/com/livro/servlets/HelloServlet.java
+++ src/br/com/livro/servlets/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String nome = req.getParameter("nome");
 		String sobrenome = req.getParameter("sobrenome");
 		
