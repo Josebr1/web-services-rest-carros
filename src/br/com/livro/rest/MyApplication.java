0f06f9abae226bc64c6c0a8f3431291867eb5690
Jose <jose.silva.br@outlook.com>
2017-04-23 02:40:50 -0300

    Web Service RESTful dos carros

--- src/br/com/livro/rest/MyApplication.java
+++ src/br/com/livro/rest/MyApplication.java
@@ -1,28 +1,16 @@
 package br.com.livro.rest;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import javax.ws.rs.core.Application;
-
-import org.glassfish.jersey.jettison.JettisonFeature;
 
 /*
  * Neste código, o método getSingletons() é responsável por adicionar funcionalidades ao 
  * Jersey.
  */
 public class MyApplication extends Application{
-	
-	@Override
-	public Set<Object> getSingletons(){
-		Set<Object> singletons = new HashSet<>();
-		// Driver do Jettison para gerar o JSON
-		singletons.add(new JettisonFeature());
-		return singletons;
-	}
-	
+
 	@Override
 	public Map<String, Object> getProperties(){
 		Map<String, Object> properties = new HashMap<>();
